72442e8be8184ff7a07da452cd89c83e0b72f02b
Mohamed Alsir <mohamed.alsir@cxunicorn.com>
2022-08-24 16:52:41 +0400

    commit

---
+++ MyShift.Notifications/Service/ServiceEng.cs
@@ -0,0 +1,41 @@
+﻿using Azure.Storage.Queues;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using MyShift.Notifications.Entitys;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShift.Notifications.Service
+{
+    public class ServiceEng : IService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogsRepository _LogsRepository;
+        public ServiceEng(IConfiguration configuration, ILogsRepository logsRepository)
+        {
+            _configuration = configuration;
+            _LogsRepository = logsRepository;
+        }
+        public async Task<string> GetDataToNotifiy()
+        {
+            var dataTobeNotifyed = await _LogsRepository.GetLogsQueries();
+            //send the notification.
+
+            var updateData= await _LogsRepository.UpdateLogsCommand(dataTobeNotifyed);
+
+
+            var queueMessage = JsonConvert.SerializeObject(dataTobeNotifyed);
+
+            return queueMessage;
+        }
+      
+    }
+}
---
+++ MyShift.Notifications/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using MyShift.Notifications;
+using MyShift.Notifications.Service;
+using Persistence.Repositories;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace MyShift.Notifications
+{
+  public  class Startup : FunctionsStartup
+    {
+       
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .AddEnvironmentVariables()
+               .Build();
+
+            builder.Services.AddScoped<IService, ServiceEng>();
+            builder.Services.AddScoped<ILogsRepository, LogsRepository>();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Data Source=.;Initial Catalog=NotificationDBV2;Integrated Security=True"));
+
+        }
+    }
+}
